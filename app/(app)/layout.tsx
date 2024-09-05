64f584008eebb15a851ccbb9e2dd8da180a0fca9
yuxxeun <itsyuxxeun@gmail.com>
2024-09-05 17:45:42 +0700

    feat: footer section

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/components/footer'
 import { Nav } from '@/components/nav'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -5,6 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div>
       <Nav />
       {children}
+      <Footer/>
     </div>
   )
 }
