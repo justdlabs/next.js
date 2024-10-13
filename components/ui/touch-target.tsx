38d3c270286e653c2140c9f153d119d9d8c78be3
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-10-13 15:22:49 +0700

    lfg

--- components/ui/touch-target.tsx
+++ components/ui/touch-target.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
+import * as React from "react"
 
 const TouchTarget = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <span
-        className="absolute pointer-events-none left-1/2 top-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 [@media(pointer:fine)]:hidden"
+        className="absolute left-1/2 top-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 [@media(pointer:fine)]:hidden"
         aria-hidden="true"
       />
       {children}
