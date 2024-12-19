a70d5f5a8f9ee193ca92c380c92a88c988fc45aa
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-12-19 16:29:05 +0700

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
